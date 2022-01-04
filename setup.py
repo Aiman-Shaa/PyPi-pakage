9ece685ed2e95294a25fcd6e86428948259ad938
Aiman-Shaa <97019515+Aiman-Shaa@users.noreply.github.com>
2022-01-04 16:58:38 +0530

    PyPi packages files

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name = 'xmath',
+    version = '0.1',
+    author = 'Abul Aiman Shaa',
+    author_email = 'shaaimanaalik@gmail.com',
+    description = 'xmath module contains many simple and advanced mathematical operations',
+    packages = setuptools.find_packages(),
+
+    classifiers = [
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
